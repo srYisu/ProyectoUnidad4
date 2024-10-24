34680ca799cae6c3c2426b484bc302abc02b3769
srYisu <jesusdelangel.19876@gmail.com>
2024-10-23 22:09:11 -0700

    puse lo visual del incio de sesion, mañana si chambeo, lo prometo

--- ProyectoUnidad4/Form1.Designer.cs
+++ ProyectoUnidad4/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.txtUsuario = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2GradientButton1 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.lblNoCuenta = new System.Windows.Forms.Label();
+            this.lblCreeAqui = new System.Windows.Forms.Label();
+            this.guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblCreeAqui);
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblNoCuenta);
+            this.guna2CustomGradientPanel1.Controls.Add(this.guna2GradientButton1);
+            this.guna2CustomGradientPanel1.Controls.Add(this.guna2TextBox2);
+            this.guna2CustomGradientPanel1.Controls.Add(this.txtUsuario);
+            this.guna2CustomGradientPanel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2CustomGradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(31)))), ((int)(((byte)(162)))));
+            this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(190)))), ((int)(((byte)(231)))));
+            this.guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(31)))), ((int)(((byte)(162)))));
+            this.guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(39)))), ((int)(((byte)(176)))));
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(543, 757);
+            this.guna2CustomGradientPanel1.TabIndex = 0;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.Image = global::ProyectoUnidad4.Properties.Resources.circulo_de_usuario__1_;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(152, 27);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(245, 200);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            this.guna2PictureBox1.UseTransparentBackground = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.txtUsuario.BorderRadius = 20;
+            this.txtUsuario.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsuario.DefaultText = "";
+            this.txtUsuario.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtUsuario.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtUsuario.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsuario.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsuario.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.txtUsuario.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUsuario.Location = new System.Drawing.Point(152, 310);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PasswordChar = '\0';
+            this.txtUsuario.PlaceholderText = "";
+            this.txtUsuario.SelectedText = "";
+            this.txtUsuario.Size = new System.Drawing.Size(245, 48);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // guna2TextBox2
+            // 
+            this.guna2TextBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2TextBox2.BorderRadius = 20;
+            this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox2.DefaultText = "";
+            this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox2.Location = new System.Drawing.Point(152, 380);
+            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.guna2TextBox2.Name = "guna2TextBox2";
+            this.guna2TextBox2.PasswordChar = '\0';
+            this.guna2TextBox2.PlaceholderText = "";
+            this.guna2TextBox2.SelectedText = "";
+            this.guna2TextBox2.Size = new System.Drawing.Size(245, 48);
+            this.guna2TextBox2.TabIndex = 2;
+            // 
+            // guna2GradientButton1
+            // 
+            this.guna2GradientButton1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2GradientButton1.BorderColor = System.Drawing.Color.White;
+            this.guna2GradientButton1.BorderRadius = 25;
+            this.guna2GradientButton1.BorderThickness = 2;
+            this.guna2GradientButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton1.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2GradientButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(39)))), ((int)(((byte)(176)))));
+            this.guna2GradientButton1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(31)))), ((int)(((byte)(162)))));
+            this.guna2GradientButton1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2GradientButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.guna2GradientButton1.Location = new System.Drawing.Point(168, 479);
+            this.guna2GradientButton1.Name = "guna2GradientButton1";
+            this.guna2GradientButton1.Size = new System.Drawing.Size(217, 75);
+            this.guna2GradientButton1.TabIndex = 3;
+            this.guna2GradientButton1.Text = "Iniciar Sesion";
+            this.guna2GradientButton1.UseTransparentBackground = true;
+            // 
+            // lblNoCuenta
+            // 
+            this.lblNoCuenta.AutoSize = true;
+            this.lblNoCuenta.BackColor = System.Drawing.Color.Transparent;
+            this.lblNoCuenta.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblNoCuenta.Location = new System.Drawing.Point(36, 598);
+            this.lblNoCuenta.Name = "lblNoCuenta";
+            this.lblNoCuenta.Size = new System.Drawing.Size(223, 28);
+            this.lblNoCuenta.TabIndex = 4;
+            this.lblNoCuenta.Text = "¿No tiene una cuenta?";
+            // 
+            // lblCreeAqui
+            // 
+            this.lblCreeAqui.AutoSize = true;
+            this.lblCreeAqui.BackColor = System.Drawing.Color.Transparent;
+            this.lblCreeAqui.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblCreeAqui.Location = new System.Drawing.Point(287, 598);
+            this.lblCreeAqui.Name = "lblCreeAqui";
+            this.lblCreeAqui.Size = new System.Drawing.Size(142, 28);
+            this.lblCreeAqui.TabIndex = 5;
+            this.lblCreeAqui.Text = "Cree una aqui";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(543, 757);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Guna.UI2.WinForms.Guna2TextBox txtUsuario;
+        private System.Windows.Forms.Label lblCreeAqui;
+        private System.Windows.Forms.Label lblNoCuenta;
     }
 }
 
--- ProyectoUnidad4/Form1.cs
+++ ProyectoUnidad4/Form1.cs
@@ -12,9 +12,70 @@ namespace ProyectoUnidad4
 {
     public partial class Form1 : Form
     {
+        Panel invisiblePanel = new Panel();
+        private string password = "Contraseña";
+        private string user = "Usuario";
         public Form1()
         {
             InitializeComponent();
+            // Agregar un control invisible que reciba el foco
+            invisiblePanel.Size = new Size(0, 0); // Hacerlo invisible
+            invisiblePanel.TabStop = false; // No es parte del orden de tabulación
+            this.Controls.Add(invisiblePanel);
+
+            // Al cargar el formulario, enfocar al panel invisible
+            this.Load += new EventHandler(Form1_Load);
+
+            // Asignar el texto de placeholder inicialmente
+            guna2TextBox2.Text = password;
+            guna2TextBox2.ForeColor = Color.Gray; // Cambiar color del texto para diferenciar el placeholder
+            txtUsuario.Text = user;
+            txtUsuario.ForeColor = Color.Gray;
+            // Suscribir los eventos Enter y Leave del TextBox
+            guna2TextBox2.Enter += new EventHandler(txtContrasena_Enter);
+            guna2TextBox2.Leave += new EventHandler(txtContrasena_Leave);
+            txtUsuario.Enter += new EventHandler(txtUsuario_Enter);
+            txtUsuario.Leave += new EventHandler(txtUsuario_Leave);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = invisiblePanel;
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == user)
+            {
+                txtUsuario.Text = ""; // Limpiar el texto
+                txtUsuario.ForeColor = Color.Black; // Cambiar el color del texto a negro
+            }
+        }
+        private void txtContrasena_Enter(object sender, EventArgs e)
+        {
+            if (guna2TextBox2.Text == password)
+            {
+                guna2TextBox2.Text = ""; // Limpiar el texto
+                guna2TextBox2.ForeColor = Color.Black; // Cambiar el color del texto a negro
+                guna2TextBox2.PasswordChar = '*'; // Activar el modo de contraseña
+            }
+        }
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsuario.Text))
+            {
+                txtUsuario.Text = user; // Restaurar el placeholder
+                txtUsuario.ForeColor = Color.Gray; // Cambiar el color del texto a gris
+            }
+        }
+        private void txtContrasena_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(guna2TextBox2.Text))
+            {
+                guna2TextBox2.PasswordChar = '\0';
+                guna2TextBox2.Text = password; // Restaurar el placeholder
+                guna2TextBox2.ForeColor = Color.Gray; // Cambiar el color del texto a gris
+
+            }
         }
     }
 }
